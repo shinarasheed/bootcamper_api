4d85d55da5db92a82f68e0b1e41c3bf0ba49e019
shina <rasheedshinaopeyemi@gmail.com>
2020-09-05 09:50:26 +0100

    completed endpoint for getting single bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,15 +3,31 @@ const Bootcamp = require("../models/Bookcamp");
 //@desc   Get all bootcamps
 //@route  GET /api/v1/bootcamps
 //@acess  Public
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ sucess: true, msg: "show all bootcamps" });
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bookcamps = await Bootcamp.find();
+    res.status(200).json({ sucess: true, data: bookcamps });
+  } catch (error) {
+    //but this is mostly a server error
+    res.status(400).json({ sucess: false });
+  }
 };
 
 //@desc   Get all bootcamps
 //@route  GET /api/v1/bootcamps/:id
 //@acess  Public
-exports.getBootcamp = (req, res, next) => {
-  res.status(200).json({ sucess: true, msg: " get a bootcamp" });
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+
+    if (!bootcamp) {
+      return res.status(400).json({ sucess: false });
+    }
+
+    res.status(200).json({ sucess: true, data: bootcamp });
+  } catch (error) {
+    res.status(500).json({ sucess: false });
+  }
 };
 
 //@desc   Create new bootcamp
