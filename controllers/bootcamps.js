453831f606bcde776173fc23ff28f69a44b9d7c9
shina <rasheedshinaopeyemi@gmail.com>
2020-09-05 22:15:07 +0100

    added get all courses and get bootcamps for a specific bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -2,7 +2,6 @@ const Bootcamp = require("../models/Bookcamp");
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
 const geocoder = require("../utils/geocoder");
-const { countDocuments } = require("../models/Bookcamp");
 
 //@desc   Get all bootcamps
 //@route  GET /api/v1/bootcamps
