b12f86bc71a181097cc6eddd65800c903e4906a2
shina <rasheedshinaopeyemi@gmail.com>
2020-09-19 17:11:16 +0100

    added nodemailer to send emails and reset password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
+const sendEmail = require("../utils/sendEmail");
 
 //@desc  Register user
 //@route  POST /api/v1/auth/register
@@ -75,11 +76,40 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
   }
 
   //Get reset token
-  user.getResetPasswordToken();
+  const resetToken = user.getResetPasswordToken();
 
   await user.save({ validateBeforeSave: false });
 
-  res.status(200).json({ success: true, data: user });
+  //create reset url
+  // const resetUrl = `${req.protocol}://${req.get(
+  //   "host"
+  // )}/api/v1/auth/resetpassword/${resetToken}`;
+
+  //create reset url
+  const resetUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/resetpassword/${resetToken}`;
+
+  const message = `You are recieving this request because you requested for a password reset. please make a request to: \n ${resetUrl} to reset your password
+  `;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: "Password reset token",
+      message,
+    });
+
+    res.status(200).json({ success: "true", data: "Email sent" });
+  } catch (error) {
+    console.log(error);
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(new ErrorResponse("Email could not be sent", 500));
+  }
 });
 
 //@desc  Get all users
